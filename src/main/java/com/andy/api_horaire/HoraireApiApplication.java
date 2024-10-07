48334b528aaf8f86f757d84e35a48dfcc3b1feed
AndyMan <josueandy5@gmail.com>
2024-10-07 09:23:26 +0200

    first commit

---
+++ src/main/java/com/andy/api_horaire/HoraireApiApplication.java
@@ -0,0 +1,13 @@
+package com.andy.api_horaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraireApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoraireApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andy/api_horaire/HoraireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.api_horaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraireApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
